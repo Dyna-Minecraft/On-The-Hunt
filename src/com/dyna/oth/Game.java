8279edc688ddb5d98e248fd7a9150a72214d6a38
Dyna-Minecraft <ryxcory@gmail.com>
2023-11-14 08:41:20 +0000

    Commit

--- src/com/dyna/oth/Game.java
+++ src/com/dyna/oth/Game.java
@@ -1,10 +1,15 @@
 package com.dyna.oth;
 
+import com.dyna.oth.gfx.Screen;
+import com.dyna.oth.gfx.SpriteSheet;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+import java.io.IOException;
 
 public class Game extends Canvas implements Runnable {
 
@@ -13,14 +18,23 @@ public class Game extends Canvas implements Runnable {
     public static final int WIDTH = HEIGHT * 16 / 9;
 
     private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
     private boolean running = false;
     private int tickCount;
+    private Screen screen;
 
 
     public void start() {
         running = true;
         new Thread(this).start();
+    }
+
+    private void init() {
+        try {
+            screen = new Screen(WIDTH, HEIGHT, new SpriteSheet(ImageIO.read(Game.class.getResourceAsStream("/icons.png"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void stop() {
@@ -36,11 +50,13 @@ public class Game extends Canvas implements Runnable {
         int ticks = 0;
         long lastTimer1 = System.currentTimeMillis();
 
+        init();
+
         while (running) {
             long now = System.nanoTime();
             unprocessed += (now - lastTime) / nsPerTick;
             lastTime = now;
-            boolean shouldRender = false;
+            boolean shouldRender = true;
             while (unprocessed >= 1) {
                 ticks++;
                 tick();
@@ -53,16 +69,15 @@ public class Game extends Canvas implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            
-            if(shouldRender) {
+
+            if (shouldRender) {
                 frames++;
                 render();
             }
 
 
-
-            if (System.currentTimeMillis()-lastTimer1>1000) {
-                lastTimer1+=1000;
+            if (System.currentTimeMillis() - lastTimer1 > 1000) {
+                lastTimer1 += 1000;
                 System.out.println(ticks + " ticks, " + frames + " fps");
                 frames = 0;
                 ticks = 0;
@@ -82,13 +97,11 @@ public class Game extends Canvas implements Runnable {
             return;
         }
 
-        for (int i = 0; i < pixels.length; i++) {
-            pixels[i] = i+tickCount;
-        }
-
+        screen.render(pixels, 0, WIDTH);
 
         Graphics g = bs.getDrawGraphics();
-        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.drawImage(image, 0, 0, WIDTH * 3, HEIGHT * 3, null);
         g.dispose();
         bs.show();
     }
@@ -96,14 +109,16 @@ public class Game extends Canvas implements Runnable {
     public static void main(String[] args) {
 
         Game game = new Game();
-        game.setMinimumSize(new Dimension(WIDTH*2, HEIGHT*2));
-        game.setPreferredSize(new Dimension(WIDTH*2, HEIGHT*2));
-        game.setMaximumSize(new Dimension(WIDTH*2, HEIGHT*2));
+        game.setMinimumSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+        game.setPreferredSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+        game.setMaximumSize(new Dimension(WIDTH * 2, HEIGHT * 2));
 
         JFrame frame = new JFrame(Game.NAME);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
         frame.setLayout(new BorderLayout());
-        frame.add(game);
+        frame.add(game, BorderLayout.CENTER);
+        frame.setContentPane(panel);
         frame.pack();
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
