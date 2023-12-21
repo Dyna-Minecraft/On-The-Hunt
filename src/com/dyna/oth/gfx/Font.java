15dc655188de917f407979a623bf0ce52f90d142
Dyna-Minecraft <ryxcory@gmail.com>
2023-12-21 11:33:21 +0000

    thx slash!

--- src/com/dyna/oth/gfx/Font.java
+++ src/com/dyna/oth/gfx/Font.java
@@ -6,12 +6,16 @@ public class Font {
             "0123456789.,!?'\"-+=/\\%()<>      " + //
             "";
 
+    private int white = (5 + 5 * 6 + 5 * 36) * 256 * 256 * 256;
+    private int black = 0;
+
     public void draw(String msg, Screen screen, int x, int y) {
         msg = msg.toUpperCase();
         for (int i = 0; i < msg.length(); i++) {
             int ix = chars.indexOf(msg.charAt(i));
             if (ix >= 0) {
-                screen.setTile(x + i, y, 0, ix + 30 * 32, (5 + 5 * 6 + 5 * 36) * 256 * 256 * 256, 0);
+                int color = msg.charAt(i) != ' ' ? white : black;
+                screen.setTile(x + i, y, 0, ix + 30 * 32, color, 0);
             }
         }
     }
